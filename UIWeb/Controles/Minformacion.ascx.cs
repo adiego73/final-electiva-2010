365e61304924e71293cee8a5b8b2ee55e11bba7a
adiego73@gmail.com <adiego73@gmail.com@a5e207bd-0a37-0007-41cb-acb2e3276562>
2010-12-09 18:28:28 +0000

    - se agrego la seccion de modificar usuario de internet, junto con algunas opciones del cliente. - ademas se modifico la ruta de la base de datos. Ahora hay que ubicarla en C:\. ! se dejaron los archivos en UIWeb/App_Data/ y UIForms/bin/Debug/ sin embargo estos NO DEBEN USARSE MAS.

--- UIWeb/Controles/Minformacion.ascx.cs
+++ UIWeb/Controles/Minformacion.ascx.cs
@@ -10,14 +10,60 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using Logic;
+using System.Collections.Generic;
+using Library.Excepciones;
+
 
 namespace UIWeb.Controles
 {
     public partial class Minformacion : System.Web.UI.UserControl
     {
+        public Usuario usuario;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            usuario = (Usuario)Session["Usuario"];
 
+            if (!IsPostBack)
+            {
+                tbApellidoNombre.Text = usuario.Cliente.Apellido + ", " + usuario.Cliente.Nombre;
+                tbApellidoNombre.Enabled = false;
+                tbUsuario.Text = usuario.User;
+                tbPassword.Text = usuario.Contrasenia;
+                tbProvincia.Text = usuario.Cliente.Provincia;
+                tbTelefono.Text = usuario.Cliente.Telefono;
+                tbMail.Text = usuario.Cliente.Mail;
+                tbDni.Text = usuario.Cliente.Dni.ToString();
+                tbDni.Enabled = false;
+                tbPuntos.Text = usuario.Cliente.CantPuntos.ToString();
+                tbPuntos.Enabled = false;
+            }
+
+        }
+
+        protected void btModificar_Click(object sender, EventArgs e)
+        {
+            usuario.User = tbUsuario.Text;
+            usuario.Contrasenia = tbPassword.Text;
+            usuario.Cliente.Provincia = tbProvincia.Text;
+            usuario.Cliente.Telefono = tbTelefono.Text;
+            usuario.Cliente.Mail = tbMail.Text;
+
+            try
+            {
+                ASupermercado.modificar(usuario);
+                ASupermercado.modificar(usuario.Cliente);
+                lException.Text = "La informacion se modifico correctamente.";
+                lException.ForeColor = new System.Drawing.Color();
+                lException.ForeColor = System.Drawing.ColorTranslator.FromHtml("#46FF96");
+            }
+            catch (ExcepcionGral exc)
+            {
+                lException.Text = exc.Message;
+                lException.ForeColor = new System.Drawing.Color();
+                lException.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+            }
         }
     }
 }
